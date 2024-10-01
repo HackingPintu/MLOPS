964681fcb55460e43de91674453b4b8a62786792
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2024-10-01 08:42:53 +0000

    New Changes

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,10 +1,10 @@
-from zenml import pipelines
+from zenml import pipeline
 from steps.ingest_data import ingest_df
 from steps.clean_data import clean_data
 from steps.evaluation import evaluate_model
 from steps.model_train import train_model
 
-@pipelines
+@pipeline
 def training_pipeline(data_path:str):
     df=ingest_df(data_path)
     clean_data(df)
